fe2fa039ff70583f1c3516b997f893d8f321eb52
jenda_horak <484526@muni.cz>
2023-10-16 15:10:02 +0200

    Test

--- src/pages/materials/materials.js
+++ src/pages/materials/materials.js
@@ -23,7 +23,7 @@ function main() {
   // LIGHT
   {
     const color = 0xffffff;
-    const intensity = 500;
+    const intensity = 1000;
     const light = new THREE.PointLight(color, intensity);
     scene.add(light);
   }
