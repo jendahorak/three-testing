1c59f4778890695bc7262610a16a51838db25f00
jenda_horak <484526@muni.cz>
2023-10-17 19:17:56 +0200

    deployed-from-script

---
+++ dist/assets/cameras-e8b4ae79.js
@@ -0,0 +1,2 @@
+import{W as c,P as i,S as m}from"./three.module-47a8c2cd.js";import{G as p,O as f}from"./dat.gui.module-dae7f470.js";function u(){new p;const e=document.querySelector("#c");new c({antialias:!0,canvas:e});const o=45,s=2,a=.1,r=100,t=new i(o,s,a,r);t.position.set(0,10,20),new m;const n=new f(t,e);n.target.set(0,5,0),n.update()}u();
+//# sourceMappingURL=cameras-e8b4ae79.js.map
