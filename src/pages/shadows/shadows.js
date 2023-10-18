44db9fe09f96add6fbb86ed3a4ccc81c614319a6
jenda_horak <484526@muni.cz>
2023-10-18 20:11:32 +0200

    add

--- src/pages/shadows/shadows.js
+++ src/pages/shadows/shadows.js
@@ -10,15 +10,26 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 
+console.log(shadowImg);
 // SCENE
 const scene = new THREE.Scene();
 
-// Ambient Light
+{
+  const skyColor = 0xb1e1ff; // light blue
+  const groundColor = 0xb97a20; // brownish orange
+  const intensity = 2;
+  const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+  scene.add(light);
+}
+
 {
   const color = 0xffffff;
   const intensity = 1;
-  const light = new THREE.AmbientLight(color, intensity);
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 10, 5);
+  light.target.position.set(-5, 0, 0);
   scene.add(light);
+  scene.add(light.target);
 }
 
 // CAMERA;
@@ -59,6 +70,54 @@ const loader = new THREE.TextureLoader();
   scene.add(mesh);
 }
 
+// SPHERES
+const shpereShadowBases = [];
+const sphereRadius = 1;
+const sphereWidthDivisisions = 32;
+const sphereHeightDivisions = 16;
+const sphereGeo = new THREE.SphereGeometry();
+
+const shadowTexture = loader.load(shadowImg);
+
+//  plane geometry for the fake shadow
+const planeSize = 1;
+const shadowGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+
+const numSpheres = 15;
+for (let i = 0; i < numSpheres; ++i) {
+  // make a base for the shadow and the sphere
+  // so they move together.
+  const base = new THREE.Object3D();
+  scene.add(base);
+
+  // add the shadow to the base
+  // note: we make a new material for each sphere
+  // so we can set that sphere's material transparency
+  // separately.
+  const shadowMat = new THREE.MeshBasicMaterial({
+    map: shadowTexture,
+    transparent: true, // so we can see the ground
+    depthWrite: false, // so we don't have to sort
+  });
+  const shadowMesh = new THREE.Mesh(shadowGeo, shadowMat);
+  shadowMesh.position.y = 0.001; // so we're above the ground slightly
+  shadowMesh.rotation.x = Math.PI * -0.5;
+  const shadowSize = sphereRadius * 4;
+  shadowMesh.scale.set(shadowSize, shadowSize, shadowSize);
+  base.add(shadowMesh);
+
+  // add the sphere to the base
+  const u = i / numSpheres; // goes from 0 to 1 as we iterate the spheres.
+  const sphereMat = new THREE.MeshPhongMaterial();
+  sphereMat.color.setHSL(u, 1, 0.75);
+  const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+  sphereMesh.position.set(0, sphereRadius + 2, 0);
+  base.add(sphereMesh);
+
+  // remember all 3 plus the y position
+  shpereShadowBases.push({ base, sphereMesh, shadowMesh, y: sphereMesh.position.y });
+}
+
 function resizeRendererToDisplaySize(renderer) {
   const canvas = renderer.domElement;
   const width = canvas.clientWidth;
@@ -79,6 +138,26 @@ function render(time) {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
   }
+
+  shpereShadowBases.forEach((shpereShadowBase, ndx) => {
+    const { base, sphereMesh, shadowMesh, y } = shpereShadowBase;
+
+    const u = ndx / shpereShadowBases.length;
+
+    // compute a position for the base. This will move
+    // both the sphere and its shadow
+    const speed = time * 0.2;
+    const angle = speed + u * Math.PI * 2 * (ndx % 1 ? 1 : -1);
+    const radius = Math.sin(speed - ndx) * 10;
+    base.position.set(Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
+
+    // yOff is a value that goes from 0 to 1
+    const yOff = Math.abs(Math.sin(time * 2 + ndx));
+    // move the sphere up and down
+    sphereMesh.position.y = y + THREE.MathUtils.lerp(-2, 2, yOff);
+    // fade the shadow as the sphere goes up
+    shadowMesh.material.opacity = THREE.MathUtils.lerp(1, 0.25, yOff);
+  });
 
   renderer.render(scene, camera);
 
